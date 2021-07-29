78ceea13e33367bcf204b1f075918f7d056dd9ee
Hemanta_Gayen <69314427+Codehemanta@users.noreply.github.com>
2021-07-29 19:19:25 +0530

    ok

---
+++ scripts/popup.js
@@ -0,0 +1,25 @@
+domReady(() => {
+  translateHTML()
+  initRateButton()
+})
+
+function domReady (callback) {
+  if (document.readyState === 'complete') {
+    callback()
+  } else {
+    window.addEventListener('load', callback, false);
+  }
+}
+
+function translateHTML (dataKey = 'message') {
+  for (const $element of document.getElementsByTagName('*')) {
+    if ($element.dataset && $element.dataset[dataKey]) {
+      $element.innerHTML = chrome.i18n.getMessage($element.dataset[dataKey])
+    }
+  }
+}
+
+
+function initRateButton() {
+  document.querySelector('.teaser').href = `https://microsoftedge.microsoft.com/addons/detail/mytube-for-youtube%E2%84%A2/jfbcahnffhbepgccnkchdacpcabbalde`;
+}
