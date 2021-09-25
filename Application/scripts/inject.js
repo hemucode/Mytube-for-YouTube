c191499f6dbf1b5d6176d2678f4b236b82848c03
Hemanta Gayen <hematagayan2000@gmail.com>
2021-09-24 23:45:07 -0700

    ok

--- Application/scripts/inject.js
+++ Application/scripts/inject.js
@@ -1,47 +1,33 @@
-//Mytube for youtube by @thegayen - https://github.com/thegayen/Mytube-for-YouTube/ 
-//License - https://github.com/thegayen/Mytube-for-YouTube/license ( JavaScript: MIT License)
-//Copywrite 2021 @thegayen
-
-window[code_xy(380)][code_xy(341)] == 'www.youtube.com' && (setInterval(autoadd, this['secend']));
-
-const secend = 500;
-const comments = ['input', '☺ȻᵾŦɆNɆSS ØVɆɌŁØȺĐɆĐ🤙', 
-                  '||||||||||˙·٠•●♥ [ тιρтσρ ] ♥●•٠·˙||||||||||', 
-                  '.ytd-mealbar-promo-renderer', 'ytd-searchboxz1', 
-                  '.ytp-ad-overlay-slot', 'Good One❤', 
-                  '😍!-!××ＦＡＢＵＬＯＵＳ**!-!😍', 
-                  '[aria-label=\'Cancel\']', 
+const textfile = ['input', '☺ȻᵾŦɆNɆSS ØVɆɌŁØȺĐɆĐ🤙', 
+                  '||||||||||˙·٠•●♥ [ тιρтσρ ] ♥●•٠·˙||||||||||', '.ytd-mealbar-promo-renderer', 
+                  'ytd-searchboxz1', '.ytp-ad-overlay-slot', 'Good One❤', 
+                  '😍!-!××ＦＡＢＵＬＯＵＳ**!-!😍', '[aria-label=\'Cancel\']', 
                   'Add a public comment... [Other options then click]', 'span', 
                   ' [ 👊•• D💪H💪A💪A💪S💪U ••😎 ] ║▌║▌║█│▌', 
                   'input[id=\'search\']', '✌||::-A’M’A’Z’I’N’G-::||✌', 
                   '#__________________BEST👍', '#player-ads', 'innerHTML', 
                   '.ytp-ad-skip-button', 'You are Super 🤩', '799739JiEsIn', 
-                  '505659BNgqDi', 'box4', 'You look gorgeous', 
-                  'ytd-guide-section-renderer', 'location', '#065fd4', 
-                  'getElementsByTagName', '🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥', 
-                  '#watch7-sidebar-ads', '<a></a>', 'click', 
-                  '1KmjkmQ', 'header ytd-playlist-panel-renderer', 
-                  '408086vstxFO', 'div[aria-label=\'Add a public comment... [Other options then click]\']', 
-                  'length', '#right-arrow', 
-                  ' /  #TOP /  #MAST #LOVELY/', 
-                  'playlist-items ytd-playlist-panel-renderer', 
-                  '👑 ☝KING:=😎', '#masthead-ad', '#d2a8ff', 
-                  '#left-arrow', 'remove', '██████████  brilliant ✔', 
+                  '505659BNgqDi', 'box4', 'You look gorgeous', 'ytd-guide-section-renderer', 
+                  'location', '#065fd4', 'getElementsByTagName', '🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥', 
+                  '#watch7-sidebar-ads', '<a></a>', 'click', '1KmjkmQ', 
+                  'header ytd-playlist-panel-renderer', '408086vstxFO', 
+                  'div[aria-label=\'Add a public comment... [Other options then click]\']', 
+                  'length', '#right-arrow', ' /  #TOP /  #MAST #LOVELY/', 
+                  'playlist-items ytd-playlist-panel-renderer', '👑 ☝KING:=😎', 
+                  '#masthead-ad', '#d2a8ff', '#left-arrow', 'remove', '██████████  brilliant ✔', 
                   'div[class=\'style-scope ytd-searchbox\']', 'ytd-search', 
-                  '😍©©©©▄▀▄ [ a͢t͢t͢r͢a͢c͢t͢i͢v͢e͢ ] ▄▀▄©©©©😍', 
-                  'Oh my God! You’re awesome', 'querySelector', 
-                  'box12', 'video', 'WoooWWoo It\'s Super', '1294lmfARR', 
-                  'This Just warms My heart ❤', 'width', 
-                  'ytd-video-renderer', 'background-color', 
-                  'ytd-rich-grid-renderer', '#left-arrow-button', 
+                  '😍©©©©▄▀▄ [ a͢t͢t͢r͢a͢c͢t͢i͢v͢e͢ ] ▄▀▄©©©©😍', 'Oh my God! You’re awesome', 
+                  'querySelector', 'box12', 'video', 'WoooWWoo It\'s Super', '1294lmfARR', 
+                  'This Just warms My heart ❤', 'width', 'ytd-video-renderer', 
+                  'background-color', 'ytd-rich-grid-renderer', '#left-arrow-button', 
                   '██████████ Perfect ✔', 'My one and only', 'Really cool🥰', 
                   'ytp-swatch-background-color', 'style-scope ytd-searchbox', 
-                  ' SO So SO Cute🥰', '#search-icon-legacy', '#0d1117', 
-                  'bodyxyz', 'innerText', 'onclick', 'classList', '#chips', 
-                  '.ad-container', '#161b22', '#58a6ff', 'setAttribute', 
-                  '██████████ Lovely looking', '██████████ Honest ✔', 
-                  'style-scope ytd-commentbox', 'add', '#ccc', '1TbMQrU', 
-                  '██████████ sweet ✔', 'getElementsByClassName', 'containeradd10x', 
+                  ' SO So SO Cute🥰', '#search-icon-legacy', '#0d1117', 'bodyxyz', 
+                  'innerText', 'onclick', 'classList', '#chips', '.ad-container', 
+                  '#161b22', '#58a6ff', 'setAttribute', '██████████ Lovely looking', 
+                  '██████████ Honest ✔', 'style-scope ytd-commentbox', 'add', '#ccc', 
+                  '1TbMQrU', '██████████ sweet ✔', 'getElementsByClassName', 
+                  'containeradd10x', 
                   'Great ✔super ✔Perfect ✔Honest ✔Beautiful ✔sweet ✔brilliant ✔', 
                   'floor', '431841EAYhsh', 'ytd-commentbox style-primary', 'SUPERBBB', 
                   '#right-arrow-button', '564AyrDRL', 'removex1', 'host', 'style', 
@@ -50,63 +36,61 @@ const comments = ['input', '☺ȻᵾŦɆNɆSS ØVɆɌŁØȺĐɆĐ🤙',
                   '653571BczKNP', 'OMG Woowwww😍😍😍', 'background', '500px', 'box1', 
                   'Nice Nice Niceeee 🤩🤩', 'aria-label'];
 
-const idea = function (codex, janina) {
-    codex = codex - 282;
-    let defence = comments[codex];
-    return defence;
+const text = function (data, _0x5d5eca) {
+    data = data - 0x11a;
+    let file = textfile[data];
+    return file;
 };
-
-const code_xy = idea;
-(function (code_xyz, cords) {
-    const abhiman = idea;
+const _0x1f528d = text;
+(function (_0x5acb1c, _0x35c027) {
+    const _0x386e83 = text;
     while (!![]) {
         try {
-            const cover = 
-            -parseInt(abhiman(375)) * 
-            -parseInt(abhiman(329)) + 
-            -parseInt(abhiman(349)) + 
-            -parseInt(abhiman(300)) * 
-            -parseInt(abhiman(339)) + 
-            -parseInt(abhiman(335)) + 
-            -parseInt(abhiman(387)) * 
-            parseInt(abhiman(346)) + 
-            -parseInt(abhiman(389)) + 
-            parseInt(abhiman(376));
-            if (cover === cords) break;
-            else code_xyz['push'](code_xyz['shift']());
+            const _0x2ee4a9 = -parseInt(_0x386e83(0x177)) * -parseInt(_0x386e83(0x149)) + -parseInt(_0x386e83(0x15d)) + -parseInt(_0x386e83(0x12c)) * -parseInt(_0x386e83(0x153)) + -parseInt(_0x386e83(0x14f)) + -parseInt(_0x386e83(0x183)) * parseInt(_0x386e83(0x15a)) + -parseInt(_0x386e83(0x185)) + parseInt(_0x386e83(0x178));
+            if (_0x2ee4a9 === _0x35c027) break;
+            else _0x5acb1c['push'](_0x5acb1c['shift']());
         } catch (_0x3e3691) {
-            code_xyz['push'](code_xyz['shift']());
+            _0x5acb1c['push'](_0x5acb1c['shift']());
         }
     }
-}(comments, 450217));
+}(textfile, 0x6dea9));
+const secend = 0x7d0;
+window[_0x1f528d(0x17c)][_0x1f528d(0x155)] == 'www.youtube.com' && (setInterval(autoadd, this['secend']));
+
 
 function autoadd() {
-    const bara = code_xy,
-    pod = document[bara(296)]('div#contenteditable-root');
-    pod && pod[bara(0x143)](bara(0x163), bara(0x16d));
-    const bal = document[bara(0x14b)]('removex1')[0x2],
-    knows = document['getElementsByClassName']('removex1')[0x0],
-    pots = document[bara(0x14b)](bara(0x154))[0x1];
-    pots && (pots[bara(0x13d)] = function () {autocmmantfun();});
-    knows && (knows[bara(0x13d)] = function () {botruncmmd();});
-    bal && bal[bara(0x122)]();
-    const tops = document[bara(0x128)](bara(0x186));
-    tops && (tops['onclick'] = function () {addautobtn();},autobuttonaction());
-    attendance();
+    const _0x2b9df5 = _0x1f528d,
+        _0x14e0ef = document[_0x2b9df5(0x128)]('div#contenteditable-root');
+    _0x14e0ef && _0x14e0ef[_0x2b9df5(0x143)](_0x2b9df5(0x163), _0x2b9df5(0x16d));
+    const _0x1cd4ed = document[_0x2b9df5(0x14b)]('removex1')[0x2],
+        _0x44992a = document['getElementsByClassName']('removex1')[0x0],
+        _0x428edd = document[_0x2b9df5(0x14b)](_0x2b9df5(0x154))[0x1];
+    _0x428edd && (_0x428edd[_0x2b9df5(0x13d)] = function () {
+        autocmmantfun();
+    });
+    _0x44992a && (_0x44992a[_0x2b9df5(0x13d)] = function () {
+        botruncmmd();
+    });
+    _0x1cd4ed && _0x1cd4ed[_0x2b9df5(0x122)]();
+    const _0x44528d = document[_0x2b9df5(0x128)](_0x2b9df5(0x186));
+    _0x44528d && (_0x44528d['onclick'] = function () {
+        addautobtn();
+    }, autobuttonaction());
 }
+
 function autocmmantfun() {}
 
 function addautobtn() {
-    const colorfull = code_xy,
-    cool = document[colorfull(0x128)](colorfull(0x16c));
-    cool && cool['remove']();
-    const coffy = '<button class=\'removex1 btnvz1\'><b>BOT</b></button><button class=\'removex1\'><b>AUTO</b></button>',
-    YouareSuper = document[colorfull(0x14b)](colorfull(0x146))[0x1b];
-    YouareSuper && (YouareSuper[colorfull(0x174)] += coffy);
+    const _0xa0905 = _0x1f528d,
+        _0x1d2dd1 = document[_0xa0905(0x128)](_0xa0905(0x16c));
+    _0x1d2dd1 && _0x1d2dd1['remove']();
+    const _0x1671eb = '<button class=\'removex1 btnvz1\'><b>BOT</b></button><button class=\'removex1\'><b>AUTO</b></button>',
+        _0x4a1541 = document[_0xa0905(0x14b)](_0xa0905(0x146))[0x1b];
+    _0x4a1541 && (_0x4a1541[_0xa0905(0x174)] += _0x1671eb);
 }
 
 function autobuttonaction() {
-    const _0x2ce64c = code_xy,
+    const _0x2ce64c = _0x1f528d,
         _0x1583e6 = document[_0x2ce64c(0x128)](_0x2ce64c(0x157))[_0x2ce64c(0x13c)],
         _0x4b5c85 = document[_0x2ce64c(0x14b)](_0x2ce64c(0x154))[0x1],
         _0x1f4b97 = document['getElementsByClassName'](_0x2ce64c(0x154))[0x0];
@@ -114,39 +98,9 @@ function autobuttonaction() {
 }
 
 function botruncmmd() {
-    const cat = code_xy,
-        dogs = [cat(0x162),
-                cat(0x15e), 
-                cat(0x176), 
-                cat(0x138), 
-                cat(0x12b), 
-                cat(0x16a), 
-                cat(0x12d), 
-                cat(0x135), 
-                cat(0x151), 
-                cat(0x126), 
-                cat(0x17f), 
-                cat(0x171), 
-                cat(0x16b), 
-                cat(0x172), '❤❤❤❤❤❤❤❤❤❤❤😎', 
-                cat(0x16f), '😯||-🇼 🇴 🇼-||😯', 
-                cat(0x11e), '❤🧡💛💚🖤💚💛🧡❤', 
-                cat(0x165), '❣❣❣❣❣❣❣❣❣❣', 
-                cat(0x11c), 
-                cat(0x144), 
-                cat(0x133), 
-                cat(0x145), '██████████ sweet ✔', 
-                cat(0x123), 
-                cat(0x14a), '██████████ nice ✔', '██████████ Beautiful ✔', 
-                cat(0x14d), 
-                cat(0x166), 
-                cat(0x17a), 
-                cat(0x127), 'Impressive', 
-                cat(0x134)];
-    botcmmmaa = dogs[Math[cat(0x14e)](Math['random']() * 
-                dogs[cat(0x11a)])], document['querySelector'](cat(0x157))['innerText'] = botcmmmaa,
-                document[cat(0x14b)](cat(0x150))[0x0][cat(0x182)](), 
-                document[cat(0x128)](cat(0x157))['innerText'] = '';
+    const _0x23754f = _0x1f528d,
+        _0x4cf963 = [_0x23754f(0x162), _0x23754f(0x15e), _0x23754f(0x176), _0x23754f(0x138), _0x23754f(0x12b), _0x23754f(0x16a), _0x23754f(0x12d), _0x23754f(0x135), _0x23754f(0x151), _0x23754f(0x126), _0x23754f(0x17f), _0x23754f(0x171), _0x23754f(0x16b), _0x23754f(0x172), '❤❤❤❤❤❤❤❤❤❤❤😎', _0x23754f(0x16f), '😯||-🇼 🇴 🇼-||😯', _0x23754f(0x11e), '❤🧡💛💚🖤💚💛🧡❤', _0x23754f(0x165), '❣❣❣❣❣❣❣❣❣❣', _0x23754f(0x11c), _0x23754f(0x144), _0x23754f(0x133), _0x23754f(0x145), '██████████ sweet ✔', _0x23754f(0x123), _0x23754f(0x14a), '██████████ nice ✔', '██████████ Beautiful ✔', _0x23754f(0x14d), _0x23754f(0x166), _0x23754f(0x17a), _0x23754f(0x127), 'Impressive', _0x23754f(0x134)];
+    botcmmmaa = _0x4cf963[Math[_0x23754f(0x14e)](Math['random']() * _0x4cf963[_0x23754f(0x11a)])], document['querySelector'](_0x23754f(0x157))['innerText'] = botcmmmaa, document[_0x23754f(0x14b)](_0x23754f(0x150))[0x0][_0x23754f(0x182)](), document[_0x23754f(0x128)](_0x23754f(0x157))['innerText'] = '';
 }
 
 
@@ -179,18 +133,15 @@ var config = {
     });
   }
 };
-
 config.load();
-
-
-
+setInterval(attendance,500);
 var sub,likebtn,liketiger,tutorial;
 function attendance() {
   tutorial = "?v=dJTeEhwUzrg";
   sub = document.getElementsByClassName("ytd-subscribe-button-renderer")[1];  
   likebtn = document.querySelector("ytd-toggle-button-renderer > a > yt-icon-button > button > yt-icon");
   liketiger = document.querySelector("ytd-toggle-button-renderer > a > yt-icon-button > button");
-	if (document.location.search==tutorial) {
+    if (document.location.search==tutorial) {
     if (!document.querySelector("yt-formatted-string.size-small")) {
     if (sub) {
     if (sub.innerText=="Abonner" || 
